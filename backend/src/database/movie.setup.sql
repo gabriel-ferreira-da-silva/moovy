c1fa6a0fbcc89df3dee819f424456951bd97cc2f
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-11-20 11:34:33 -0300

    feat: added database setup file wich  creates moovy database and movies table

---
+++ backend/src/database/movie.setup.sql
@@ -0,0 +1,50 @@
+DO
+$$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_database WHERE datname = 'moovydb'
+    ) THEN
+        PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE moovydb');
+    END IF;
+END
+$$;
+
+\c moovydb
+
+DROP TABLE IF EXISTS movies;
+CREATE TABLE movies (
+    imdbID TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    year TEXT,
+    rated TEXT,
+    released TEXT,
+    runtime TEXT,
+    genre TEXT,
+    director TEXT,
+    writer TEXT,
+    actors TEXT,
+    plot TEXT,
+    language TEXT,
+    country TEXT,
+    awards TEXT,
+    poster TEXT,
+    metascore TEXT,
+    imdbRating TEXT,
+    imdbVotes TEXT,
+    type TEXT,
+    dvd TEXT,
+    boxOffice TEXT,
+    production TEXT,
+    website TEXT,
+    response TEXT,
+    error TEXT
+);
+
+DROP TABLE IF EXISTS ratings;
+CREATE TABLE IF NOT EXISTS ratings (
+    imdbID TEXT NOT NULL,
+    source TEXT,
+    value TEXT,
+    PRIMARY KEY (imdbID, source), 
+    FOREIGN KEY (imdbID) REFERENCES movies(imdbID) ON DELETE CASCADE
+);
