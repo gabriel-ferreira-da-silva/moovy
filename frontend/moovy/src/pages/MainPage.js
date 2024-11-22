2b42ab596a9225d422957bc001b94bce75048c2e
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-11-21 23:29:45 -0300

    added moviesPanel to Mainpage.js

--- frontend/moovy/src/pages/MainPage.js
+++ frontend/moovy/src/pages/MainPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import SearchPanel from "../components/SearchPanel/SearchPanel";
+import MoviesPanel from "../components/MoviesPanel/MoviesPanel";
 export default function MainPage(){
+    const [movies,setMovies] = useState([]);
+    useEffect(()=>{
+
+    },[]);
     return(
         <div>
-            <SearchPanel></SearchPanel>    
+            <SearchPanel></SearchPanel> 
+            
+            <MoviesPanel
+            movies={movies}
+            ></MoviesPanel>   
         </div>
     );
 }
