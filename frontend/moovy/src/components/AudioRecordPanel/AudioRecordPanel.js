ec4f30ebb954e276da1dc34bc2d7da860bef024f
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-11-25 18:41:52 -0300

    feat: now audio review can be played and style was added to audioRecordPanel

--- frontend/moovy/src/components/AudioRecordPanel/AudioRecordPanel.js
+++ frontend/moovy/src/components/AudioRecordPanel/AudioRecordPanel.js
@@ -78,27 +78,42 @@ export default function AudioRecordPanel({movie}) {
 
     return (
         <div>
-            <h1>Audio Recorder</h1>
+            <h4>Record audio</h4>
             {error && <p style={{ color: 'red' }}>{error}</p>}
 
             <div className={style.mainPanel}>
                 {!isRecording ? (
                     <div onClick={startRecording} className={style.appButton}>
-                        <img src={microphoneIcon}></img>
+                        <svg
+                            fill="currentColor"
+                            height="20px"
+                            width="20px"
+                            viewBox="0 0 512 512"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M439.5,236c0-11.3-9.1-20.4-20.4-20.4s-20.4,9.1-20.4,20.4c0,70-64,126.9-142.7,126.9-78.7,0-142.7-56.9-142.7-126.9 0-11.3-9.1-20.4-20.4-20.4s-20.4,9.1-20.4,20.4c0,86.2 71.5,157.4 163.1,166.7v57.5h-23.6c-11.3,0-20.4,9.1-20.4,20.4 0,11.3 9.1,20.4 20.4,20.4h88c11.3,0 20.4-9.1 20.4-20.4 0-11.3-9.1-20.4-20.4-20.4h-23.6v-57.5c91.6-9.3 163.1-80.5 163.1-166.7z" />
+                            <path d="M256,323.5c51,0 92.3-41.3 92.3-92.3v-127.9c0-51-41.3-92.3-92.3-92.3s-92.3,41.3-92.3,92.3v127.9c0,51 41.3,92.3 92.3,92.3zm-52.3-220.2c0-28.8 23.5-52.3 52.3-52.3s52.3,23.5 52.3,52.3v127.9c0,28.8-23.5,52.3-52.3,52.3s-52.3-23.5-52.3-52.3v-127.9z" />
+                        </svg>
+                        <div>record</div>
                     </div>
                 ) : (
                     <div onClick={stopRecording} className={style.appButton}>
-                        <img src={stopIcon}></img>
+                        <svg width="20px" height="20px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM8.58579 8.58579C8 9.17157 8 10.1144 8 12C8 13.8856 8 14.8284 8.58579 15.4142C9.17157 16 10.1144 16 12 16C13.8856 16 14.8284 16 15.4142 15.4142C16 14.8284 16 13.8856 16 12C16 10.1144 16 9.17157 15.4142 8.58579C14.8284 8 13.8856 8 12 8C10.1144 8 9.17157 8 8.58579 8.58579Z"/>
+                        </svg>
+                        <div>stop</div>
                     </div>
                 )}
             </div>
 
             {audioURL && (
                 <div className={style.audioPlayer}>
-                    <h2>Playback</h2>
                     <audio src={audioURL} controls></audio>
                     <div onClick={saveRecording} className={style.appButton}>
-                        <img src={saveIcon}></img>
+                        <svg width="20px" height="20px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M18.1716 1C18.702 1 19.2107 1.21071 19.5858 1.58579L22.4142 4.41421C22.7893 4.78929 23 5.29799 23 5.82843V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H18.1716ZM4 3C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21L5 21L5 15C5 13.3431 6.34315 12 8 12L16 12C17.6569 12 19 13.3431 19 15V21H20C20.5523 21 21 20.5523 21 20V6.82843C21 6.29799 20.7893 5.78929 20.4142 5.41421L18.5858 3.58579C18.2107 3.21071 17.702 3 17.1716 3H17V5C17 6.65685 15.6569 8 14 8H10C8.34315 8 7 6.65685 7 5V3H4ZM17 21V15C17 14.4477 16.5523 14 16 14L8 14C7.44772 14 7 14.4477 7 15L7 21L17 21ZM9 3H15V5C15 5.55228 14.5523 6 14 6H10C9.44772 6 9 5.55228 9 5V3Z"/>
+                        </svg>
+                        <div>save</div>
                     </div>
                 </div>
             )}
